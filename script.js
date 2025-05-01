824f9f68c0e69da80bdcc2c66003b91f5fbc1bce
KariMckinney <ajpetrikov@gmail.com>
2025-05-01 11:42:32 -0500

    Fixed code

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
-let nameInputElement = document.getElementById("name-input");
-let nameOutputElement = document.getElementById("name-output");
-nameInputElement.addEventListener("submit", nameInputEventHandler);
 
-function nameInputEventHandler(event) {
+const form = document.getElementById("form");
+const orderTable = document.getElementById("table");
+
+const fname = document.getElementById("fname");
+const lname = document.getElementById("fname");
+const pizzaSize = document.getElementById("pizza-size");
+const pizzaTopping = document.getElementsByClassName("class");
+
+function formClickHandler(event) {
     event.preventDefault();
-nameOutputElement.innerHTML = `Hello there ${event.target["name-output"].value}`
-event.target.reset();
+
+const table = document.createElement("tr")
+table.innerHTML = table.value
+const fname = document.createElement("td")
+fname.innerHTML = fname.value
+const lname = document.createElement("td")
+lname.innerHTML = lname.value
+const pizzaSize = document.createElement("td")
+pizzaSize.innerHTML = pizzaSize.value
+const pizzaTopping = document.createElement("td")
+pizzaTopping.innerHTML = pizzaTopping.value
+
+table.appendChild(fname)
+table.appendChild(lname)
+table.appendChild(pizzaSize)
+table.appendChild(pizzaTopping)
+
+orderTable.appendChild(table)
 }
